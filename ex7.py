11aa109e899026633708394bc8be5c6dc3eb5f57
juanlucs <ls2699120@gmail.com>
2026-03-05 21:12:30 -0300

    Add volume calculation for a box in ex7.py

---
+++ ex7.py
@@ -0,0 +1,5 @@
+comprimento = float(input("comprimento: "))
+largura = float(input("largura: "))
+altura = float(input("altura: "))
+volume = comprimento * largura * altura
+print("Volume da caixa:", volume)
